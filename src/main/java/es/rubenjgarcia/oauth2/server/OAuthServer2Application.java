33794e684ff8b9554a493564ebb442c5c8101343
Ruben J Garcia <rubenjgarciab@gmail.com>
2016-12-02 19:58:09 +0100

    It's alive!!!

---
+++ src/main/java/es/rubenjgarcia/oauth2/server/OAuthServer2Application.java
@@ -0,0 +1,12 @@
+package es.rubenjgarcia.oauth2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthServer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthServer2Application.class, args);
+    }
+}
