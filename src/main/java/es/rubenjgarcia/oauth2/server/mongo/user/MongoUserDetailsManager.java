611638379326ab3492ada174965a0f299c6a2dea
Ruben J Garcia <rubenjgarciab@gmail.com>
2016-12-13 08:11:42 +0100

    Remove groups

--- src/main/java/es/rubenjgarcia/oauth2/server/mongo/user/MongoUserDetailsManager.java
+++ src/main/java/es/rubenjgarcia/oauth2/server/mongo/user/MongoUserDetailsManager.java
@@ -3,7 +3,6 @@ package es.rubenjgarcia.oauth2.server.mongo.user;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.provisioning.GroupManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
@@ -11,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class MongoUserDetailsManager implements UserDetailsManager, GroupManager {
+public class MongoUserDetailsManager implements UserDetailsManager {
 
     private UserRepository userRepository;
 
@@ -23,56 +22,6 @@ public class MongoUserDetailsManager implements UserDetailsManager, GroupManager
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findOne(username);
         return Optional.ofNullable(user).orElseThrow(() -> new UsernameNotFoundException(username + " not exists"));
-    }
-
-    @Override
-    public List<String> findAllGroups() {
-        throw new NotImplementedException(); //TODO
-    }
-
-    @Override
-    public List<String> findUsersInGroup(String groupName) {
-        throw new NotImplementedException(); //TODO
-    }
-
-    @Override
-    public void createGroup(String groupName, List<GrantedAuthority> authorities) {
-        throw new NotImplementedException(); //TODO
-    }
-
-    @Override
-    public void deleteGroup(String groupName) {
-        throw new NotImplementedException(); //TODO
-    }
-
-    @Override
-    public void renameGroup(String oldName, String newName) {
-        throw new NotImplementedException(); //TODO
-    }
-
-    @Override
-    public void addUserToGroup(String username, String group) {
-        throw new NotImplementedException(); //TODO
-    }
-
-    @Override
-    public void removeUserFromGroup(String username, String groupName) {
-        throw new NotImplementedException(); //TODO
-    }
-
-    @Override
-    public List<GrantedAuthority> findGroupAuthorities(String groupName) {
-        throw new NotImplementedException(); //TODO
-    }
-
-    @Override
-    public void addGroupAuthority(String groupName, GrantedAuthority authority) {
-        throw new NotImplementedException(); //TODO
-    }
-
-    @Override
-    public void removeGroupAuthority(String groupName, GrantedAuthority authority) {
-        throw new NotImplementedException(); //TODO
     }
 
     @Override
